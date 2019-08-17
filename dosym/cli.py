4f321e9e865f55d6bbe13e5637f025852cf398ad
samjdixon <samljdixon@gmail.com>
2019-08-17 14:41:50 -0400

    Adds type hints to multiple functions Changes naming scheme Creates additional functions for spaghetti

--- dosym/cli.py
+++ dosym/cli.py
@@ -7,13 +7,14 @@ from dosym import symlink
 import toml
 
 logger = logging.create_cli_logger()
-logger.info("----")
-logger.info("CLI logger successfully created")
-logger.info("----")
+logger.info("////////////////////////////////")
+logger.info("Start of new run")
+logger.info("////////////////////////////////")
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(description="Dosym. Easily create and remove symlinks")
+def create_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+            description="Dosym. Easily create and remove symlinks")
     parser.add_argument(
             "files", 
             metavar="FILE", 
@@ -31,36 +32,55 @@ class ConfigFileInputs:
         self.symlinks = symlinks
         self.localpath = localpath
 
-def file_parser(inputfiles):
+def toml_file_parser(inputfiles: str) -> dict:
     return toml.load(inputfiles)
 
-def process_inputs():
-    # TODO put if elif else statement from cli in here
-    pass
+def toml_stdin_parser() -> dict:
+    temp = ""
+    for line in sys.stdin:
+        temp += line
 
-"Main control flow function of CLI module"
+    input_file_data = toml.loads(temp)
+    return input_file_data
+
+# Function to process inputs base on Input Type
+# If file input -> toml_file_parser
+# If stdin input -> toml_stdin_parser
+def process_inputs(args: argparse.Namespace) -> dict:
+    if args.files:
+       # TODO Error handling and logging for non TOML files
+       input_file_data = toml_file_parser(args.files[0])
+
+       logger.debug(input_file_data)
+       logger.debug("args.files true")
+    elif sys.stdin.isatty():
+        # TODO 
+        # Error handling around no input given
+        # throw error, exit, let user know why
+        print("No input file given and no input received")
+        print("Exiting")
+        exit()
+    else:
+        # TODO
+        # error handling around this?
+        # what if input from stdin is not proper toml
+        # Throw error, exit, let user know why
+        # Log and throw error
+        input_file_data = toml_stdin_parser()
+        logger.debug(input_file_data)
+    
+    return input_file_data
+
+  # TODO put if elif else statement from cli in here
+
+#Main control flow function of CLI module"
 def cli():
     args = create_parser()
 
     logger.debug("Logging Args")
     logger.debug(args)
 
-    if args.files:
-        # TODO Error handling and logging for non TOML files
-        input_file_data = file_parser(args.files[0])
-
-        logger.debug(data)
-        logger.debug("args.files true")
-    elif sys.stdin.isatty():
-        exit()
-    else:
-        temp = ""
-        for line in sys.stdin:
-            temp += line
-
-        input_file_data = toml.loads(temp)
-
-        logger.debug(input_file_data)
+    input_file_data = process_inputs(args)
 
     ConfigInputs = ConfigFileInputs(
             input_file_data['symlinks'], 
@@ -69,8 +89,18 @@ def cli():
     logger.debug(ConfigInputs)
 
     symlink_object = symlink.DoSymlinks()
+
+    # TODO 
+    # This needs to be an if statement:
+    # If localpath = True: Merge localpath and key
+    # Else key is just based off relative path
+    # First option will be preferred. Second will require tons of error
+    # handling and boilerplate code
     for key,val in ConfigInputs.symlinks.items():
-        symlink_object.add_symlink(key, val)
+        join_char = "/"
+        join_seq = (ConfigInputs.localpath['local_source'], key)
+        joined_key = join_char.join(join_seq)
+        symlink_object.add_symlink(joined_key, val)
 
     print(symlink_object.symlinks)
 
