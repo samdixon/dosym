027438b5e8bc941c9b2ec7a8922ead7673d6d8a1
sjdsh <samljdixon@gmail.com>
2019-09-03 08:01:58 -0700

    Adds toml parsing to inputs

--- dosym/inputs.py
+++ dosym/inputs.py
@@ -1,3 +1,46 @@
+import sys
+import logging
+import toml
+
+logger = logging.getLogger(__name__)
+
+def toml_file_parser(inputfiles: str) -> dict:
+    return toml.load(inputfiles)
+
+def toml_stdin_parser() -> dict:
+    temp = ""
+    for line in sys.stdin:
+        temp += line
+
+    return toml.loads(temp)
+
+# Function to gather inputs based on type
+# If file -> toml_file_parser
+# If stdin -> toml_stdin_parser()
+def gather_inputs(args) -> dict:
+    if args.files:
+       # TODO Error handling and logging for non TOML files
+       input_data = toml_file_parser(args.files[0])
+
+       logger.debug("File Input Data: {}".format(input_data))
+    elif sys.stdin.isatty():
+        # TODO 
+        # Error handling around no input given
+        # throw error, exit, let user know why
+        print("No input file given and no input received")
+        print("Exiting")
+        exit()
+    else:
+        # TODO
+        # error handling around this?
+        # what if input from stdin is not proper toml
+        # Throw error, exit, let user know why
+        # Log and throw error
+        input_data = toml_stdin_parser()
+        logger.debug("Stdin Input Data: {}".format(input_data))
+    
+    return input_data
+
 class InputDataTransformer:
     def __init__(self, symlinks, optional):
         self.symlinks = symlinks
@@ -10,7 +53,7 @@ class InputDataTransformer:
         
         if self.local_path != None:
             self._join_local_path_and_key()
-
+    
     def _parse_optional_inputs(self):
         if 'git_remote' in self.optional:
             self.git_remote = self.optional['git_remote']
