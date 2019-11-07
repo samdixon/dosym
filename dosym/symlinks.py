4a6375cfe64189ed4ab520718eab397e2275fd63
samdixon <sdixon@posteo.net>
2019-11-06 21:53:27 -0500

    removes old symlinks class and old helper function. Renames new helper function

--- dosym/symlinks.py
+++ dosym/symlinks.py
@@ -8,10 +8,6 @@ logger = logging.getLogger(__name__)
 """
 Simple helper function that adds symlinks to Symlinks class
 """
-def add_symlinks_helper(symlink_object, processed_input_data) -> None:
-    for key, val in processed_input_data.localpath_symlinks.items():
-        symlink_object.add_symlink(key, val)
-
 def add_symlinks_helper2(processed_input_data) -> list: 
     l = list()
     for key, val in processed_input_data.localpath_symlinks.items():
@@ -21,33 +17,6 @@ def add_symlinks_helper2(processed_input_data) -> list:
     return l
 
 
-class Symlinks:
-    def __init__(self):
-        self.symlinks = {}
-        self.validated_symlinks = {}
-        self.invalidated_symlinks = {}
-
-    def add_symlink(self, symlink_key, symlink_val):
-        if self._validate_symlink_key(symlink_key):
-            self.validated_symlinks[symlink_key] = symlink_val
-        else:
-            self.invalidated_symlinks[symlink_key] = symlink_val
-
-    def _validate_symlink_key(self, symlink_key) -> bool:
-        return os.path.exists(os.path.expanduser(symlink_key))
-
-    def _validate_symlink_val(self, symlink_val) -> bool:
-        pass
-
-    def create_symlinks(self):
-        for key, val in self.validated_symlinks.items():
-            try:
-                os.symlink(os.path.expanduser(key), os.path.expanduser(val))
-            except FileNotFoundError as e:
-                pass
-            except FileExistsError as e:
-                pass
-            
 class Symlink():
     """Class to create a single instance of a Symlink"""
     def __init__(self, src, dest):
