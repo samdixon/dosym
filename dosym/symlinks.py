bf8dcb17eab8b9a00f2f4876066c5caefee9c632
samdixon <sdixon@posteo.net>
2020-03-06 16:53:52 -0500

    updates symlinks
    changes buf variable to symlinks_buffer for naming purposes
    adds CREATED,FAILED,BROKEN status references to Symlink class.
    adds status state to Symlink
    Symlink.create functions no longer print output. Instead update the
    status.

--- dosym/symlinks.py
+++ dosym/symlinks.py
@@ -9,17 +9,22 @@ def add_symlinks_helper(processed_input_data):
     """
     Simple helper function that adds symlinks to Symlinks class
     """
-    buf = list()
+    symlinks_buffer = list()
     for key, val in processed_input_data.localpath_symlinks.items():
-        buf.append(Symlink(key,val))
+        symlinks_buffer.append(Symlink(key,val))
         logger.debug(f"Adds {key}: {val} to Symlinks List")
 
-    logger.debug(f"Symlinks: {buf}")
-    return buf
+    logger.debug(f"Symlinks: {symlinks_buffer}")
+    return symlinks_buffer
 
 
-class Symlink():
+class Symlink(object):
     """Class to create a single instance of a Symlink"""
+
+    CREATED = "created"
+    FAILED = "failed"
+    BROKEN = "broken"
+
     def __init__(self, src, dest):
         self.src = src
         self.dest = dest
@@ -31,6 +36,7 @@ class Symlink():
         if self.is_valid_src:
             self.file_type = self._check_file_type() 
         self.dest_already_symlink = False
+        self.status = "null"
 
     def _get_absolute_path(self, item):
         return os.path.expanduser(item)
@@ -69,25 +75,34 @@ class Symlink():
     def _simple_create(self):
         try:
             os.symlink(self.absolute_src, self.absolute_dest)
-            print(f"{self.src} --> {self.dest}")
+            self.status = self.CREATED
         except FileExistsError as e:
-            logger.debug(f"{e}")
+            self.status = self.FAILED
+            logger.error(f"{e}")
 
     def _force_create(self):
         try:
             subprocess.call(
                     f"ln -sfn {self.absolute_src} {self.absolute_dest}", 
                     shell=True)
-            print(f"{self.src} --> {self.dest}")
-        except:
+            if self.is_valid_src:
+                self.status = self.CREATED
+            elif not self.is_valid_src:
+                self.status = self.BROKEN
+            else:
+                pass
+        except Exception as e:
+            self.status = self.FAILED
+            logger.error(f"{e}")
             pass
 
     def create(self, force):
         if force:
+            logger.debug(
+                    f"Attempting to force create link {self.src} {self.dest}")
             self._force_create()
-            logger.debug(f"Created link {self.src} {self.dest}")
         else:
+            logger.debug(f"Attempting to create link {self.src} {self.dest}")
             self._simple_create()
-            logger.debug(f"Created link {self.src} {self.dest}")
 
 
