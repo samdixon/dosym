f83cb294bfc309cda76f30e8043032e9b7fd8961
samdixon <sdixon@posteo.net>
2020-03-13 09:52:53 -0400

    cli changes
    Adds a few features and flags. I implemented a version flag with a
    corresponding version variable. I also implemented a --generate-config
    flag that allows the user to generate blank config template to use for
    implementing their own symlinks. Both of these have functions to check
    if the user used the flag. Both exit.
    
    Also renamed cli.cli function to cli.main.

--- dosym/cli.py
+++ dosym/cli.py
@@ -2,9 +2,11 @@ import argparse
 import sys
 import logging
 import dosym.symlinks as symlinks
+import dosym.config_generator as config_generator
 import dosym.inputs as inputs
 import dosym.output as output
 
+__version__ = '0.0.2'
 logger = logging.getLogger(__name__)
 
 def create_parser(optional=None):
@@ -13,7 +15,7 @@ def create_parser(optional=None):
             "Dosym. Easily create and remove symbolic links with a toml file.")
     parser.add_argument(
             "files",
-            metavar="FILE",
+            metavar="file",
             nargs="*",
             help="One or more config files")
     parser.add_argument(
@@ -27,6 +29,16 @@ def create_parser(optional=None):
             "--debug", 
             help="Enable Debug", 
             action="store_true")
+    parser.add_argument(
+            "--generate-config",
+            help="Generate a blank config file in current directory",
+            action="store_true"
+            )
+    parser.add_argument(
+            "--version",
+            help="Print Version",
+            action="store_true"
+            )
     args = parser.parse_args(optional)
     return args
 
@@ -41,9 +53,24 @@ def check_debug_mode(args):
         logging.basicConfig(level=logging.INFO)
         return False 
 
-def cli():
+def check_generate_config_flag(args):
+    if args.generate_config:
+        generate_config_file(args)
+
+def generate_config_file(args):
+    config_generator.generate()
+
+def check_version_flag(args):
+    if args.version:
+        print(__version__)
+        sys.exit(0)
+
+def main() -> int:
     args = create_parser()
     check_debug_mode(args)
+    check_generate_config_flag(args)
+    check_version_flag(args)
+
 
     input_data = inputs.process(args)
 
