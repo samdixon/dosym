b56252b64c790eb6a3efe63a0268aabc7e458bd1
samdixon <sdixon@posteo.net>
2020-03-13 10:11:39 -0400

    output updates
    This module is still in the early phases. Added unicode characters to
    the base Terminal class. In the near future this class will be used to
    store information about the users terminal to provide better and clearer
    output depending on the situation. EG if a terminal doesn't provide
    unicode character support, this class will be able to tell.
    
    Adds super inheritance to the Outputs class. This inherits from Terminal
    base class. Depending on the symlinks status this class also adjusts the
    symbols that are printed to the user. WIP

--- dosym/output.py
+++ dosym/output.py
@@ -1,5 +1,10 @@
 class Terminal(object):
-    pass
+    check = u'\u2714' 
+    error = u'\u2715'
+    broken = u'\u2718'
+    arrow = u'\u21D2'
+    broken_arrow = u'\u21CF'
+    under_arrow = u'\u21AA'
 
 
 class tcolors:
@@ -15,24 +20,34 @@ class tcolors:
 class Outputs(Terminal):
     def __init__(self, symlinks):
         self.symlinks = symlinks
+        super().__init__()
 
     def symlink_output(self):
         for symlink in self.symlinks:
+            arrow = self.arrow
             if symlink.status == symlink.CREATED:
                 color = tcolors.OKGREEN
+                status = self.check
             elif symlink.status == symlink.BROKEN:
                 color = tcolors.WARNING
+                status = self.broken
+                arrow = self.broken_arrow
             elif symlink.status == symlink.FAILED:
                 color = tcolors.FAIL
+                status = self.error
             print(
                     "[" + 
                     color + 
-                    f"{symlink.status}" + 
+                    f"{status}" + 
                     tcolors.ENDC +
                     "]",
-                    f"{symlink.src} -> {symlink.dest}")
+                    f"{symlink.src} " + 
+                    arrow +
+                    f" {symlink.dest}")
             
             if symlink.status == symlink.BROKEN:
-                print("    - Warning: Symlink created, but source file does not exist")
+                print(f"  {self.under_arrow} Warning: Symlink created, but source file does not exist")
+            elif symlink.status == symlink.FAILED:
+                print(f"  - {symlink.error_message}")
 
 
